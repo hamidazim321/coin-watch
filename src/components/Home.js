8563993f533293d4dd6c77182b45818c64e67e4d
Hamid Azim <hamidazim321@gmail.com>
2023-09-04 17:25:39 +0500

    Add a search filter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCoins } from '../redux/Coins/coins';
 import Coin from './Coin';
@@ -7,9 +7,15 @@ import Styles from '../styles/Home.module.css';
 const Home = () => {
   const dispatch = useDispatch();
   const { coins, pending, error } = useSelector((state) => state.coins);
+  const [search, setSearch] = useState('');
   useEffect(() => {
     dispatch(fetchCoins());
   }, [dispatch]);
+
+  const handleSearch = () => (
+    coins.filter((coin) => coin.symbol.toLowerCase().startsWith(search)
+    || coin.name.toLowerCase().startsWith(search))
+  );
 
   if (pending) {
     return (
@@ -26,8 +32,16 @@ const Home = () => {
   }
 
   return (
-    <div className={Styles.coins}>
-      {coins.map((coin) => <Coin key={coin.nameid} props={coin} />)}
+    <div className={Styles.Home}>
+      <input
+        className={Styles.search}
+        type="text"
+        onChange={(e) => { setSearch(e.target.value.toLowerCase()); }}
+        placeholder="Search Coin"
+      />
+      <div className={Styles.coins}>
+        {handleSearch().map((coin) => <Coin key={coin.nameid} props={coin} />)}
+      </div>
     </div>
   );
 };
