5f6c416e1b77d65dc47d7d227bccf95aec0eed31
Hamid Azim <hamidazim321@gmail.com>
2023-09-05 15:03:50 +0500

    Fetch Coin Details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
+import { COIN_DETAILS_URL } from './API';
 
 const DetailsPage = () => {
   const { id } = useParams();
+  const [details, setDetails] = useState();
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const req = await fetch(`${COIN_DETAILS_URL}${id}`);
+        const res = await req.json();
+        setDetails(res);
+      } catch {
+        setDetails('ERROR');
+      }
+    };
+    fetchDetails();
+  }, []);
   return (
     <div>
       <NavLink to="/">Back</NavLink>
-      <p>{`Details ${id}`}</p>
+      <p>{JSON.stringify(details)}</p>
     </div>
 
   );
