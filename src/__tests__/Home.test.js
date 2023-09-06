52c5f0b8ce7fea6229f39acaeedbdebe97703910
Hamid Azim <hamidazim321@gmail.com>
2023-09-06 13:29:52 +0500

    Add unit test for Home component

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,34 +1,55 @@
-import { render } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import React from 'react'
-import { mockCoins } from './__mocks__/mockCoins'
-import { MemoryRouter } from 'react-router-dom'
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; 
+import React from 'react';
+import { mockCoins } from './__mocks__/mockCoins';
+import { MemoryRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
-import { Provider } from 'react-redux'
-import Home from '../components/Home'
+import { Provider } from 'react-redux';
+import Home from '../components/Home';
 
 const mockStore = configureStore([]);
 
 describe('Home Component', () => {
-  let store; 
+  let store;
+  let home;
+
   beforeEach(() => {
     store = mockStore({
       coins: {
         coins: mockCoins,
         pending: false,
         error: false,
-      }
-    })
-  })
+      },
+    });
+    home = render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      </MemoryRouter>
+    );
+  });
 
   test('Should render correctly', () => {
-    const home = render(
-      <MemoryRouter>
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    </MemoryRouter>
-    )
-    expect(home).toMatchSnapshot()
-  })
-})
+    expect(home).toMatchSnapshot();
+  });
+
+  test('Should filter Coins on the Homepage Correctly', async () => {
+    const searchBar = home.getByPlaceholderText('Search Coin');
+    const coins = home.container.getElementsByClassName('coin');
+
+    // Initial Coins displayed are 7
+    expect(searchBar.value).toBe('');
+    expect(coins.length).toBe(7);
+
+    // Search for a coin name
+    fireEvent.change(searchBar, { target: { value: 'Bitcoin' } });
+
+    // Wait for the component to re-render after filtering
+    await waitFor(() => {
+      const filteredCoins = home.container.getElementsByClassName('coin');
+      // The updated coins should be 3
+      expect(filteredCoins.length).toBe(1);
+    });
+  });
+});
