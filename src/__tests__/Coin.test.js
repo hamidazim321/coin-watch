d1fea5821b71f2cccb0187b47e954782a236314f
Hamid Azim <hamidazim321@gmail.com>
2023-09-06 12:43:37 +0500

    Add snapshot test for Coin component

---
+++ src/__tests__/Coin.test.js
@@ -0,0 +1,17 @@
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import React from 'react'
+import Coin from '../components/Coin'
+import { mockCoins } from './__mocks__/mockCoins'
+import { MemoryRouter } from 'react-router-dom'
+
+describe('Coin Card', () => {
+  test('Should render correctly', ()=>{
+    const coin = render(
+      <MemoryRouter>
+        <Coin props={mockCoins[0]}/>
+      </MemoryRouter>
+    )
+    expect(coin).toMatchSnapshot()
+  })
+})
