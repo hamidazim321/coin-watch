4364a1d71df838a98d68e6b67250ebd9836b5f41
Hamid Azim <hamidazim321@gmail.com>
2023-09-07 13:27:48 +0500

    Fix fetch request for data

--- src/components/API.js
+++ src/components/API.js
@@ -1,2 +1,2 @@
-export const COINS_URL = ' https://api.coinlore.net/api/tickers/?start=0&limit=100';
+export const COINS_URL = ' https://api.coinlore.net/api/tickers/?start=0&limit=90';
 export const COIN_DETAILS_URL = 'https://api.coinlore.net/api/ticker/?id=';
